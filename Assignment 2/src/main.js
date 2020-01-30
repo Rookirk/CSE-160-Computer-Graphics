4a538f71e1a492edb51fe8c17c46c494f9fb29cf
Rookirk <hwu24110@gmail.com>
2020-01-30 01:46:28 -0800

    rotationMatrix is added. Additionally did some restructing and renaming of variables

--- Assignment 2/src/main.js
+++ Assignment 2/src/main.js
@@ -3,10 +3,10 @@
 var VSHADER_SOURCE = `
     attribute vec4 a_Position;
     attribute vec4 a_Color;
-    uniform mat4 u_TransformMatrix;
+    uniform mat4 u_ModelMatrix;
     varying vec4 v_Color;
     void main() {
-      gl_Position = u_TransformMatrix * a_Position;
+      gl_Position = u_ModelMatrix * a_Position;
       v_Color = a_Color;
     }`;
 
@@ -24,11 +24,16 @@ var canvas;
 var gl;
 var a_Position;
 var a_Color;
-var u_TransformMatrix;
+var u_ModelMatrix;
 
-var g_Points;
-var vertices;
-var transformMatrix;
+var vertexArr;
+var vertsPerShapeArr;
+var shapesPerPartArr;
+
+var modelMatrix;
+var rotationMatrix;
+var scaleMatrix;
+var translateMatrix;
 
 function main() {
     // Retrieve HTML elements
@@ -49,25 +54,41 @@ function main() {
     getStorageLocations();
 
     // Initialize vertex arrays
-    g_Points = [];
-    vertices = new Float32Array(g_Points);
-
-    transformMatrix = new Matrix4();
+    vertexArr = [];
 
     initVertexBuffers();
 
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    createCube(0,0,0,.1,255,255,255);
+    modelMatrix = new Matrix4();
+    //modelMatrix.setPerspective(30, 1, 1, 100);
+    //modelMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+    
+    rotationMatrix = new Matrix4();
+    rotationMatrix.setRotate(-45,1,0,0);
+    modelMatrix.multiply(rotationMatrix);
 
-    vertices = new Float32Array(g_Points);
+    scaleMatrix = new Matrix4();
+
+    translateMatrix = new Matrix4();
+
+    vertsPerShapeArr = [];
+    shapesPerPartArr = [2,3];
+
+    createCube(   0,   0,   0,   .1,   .1,   .1,255,255,255); // white
+    createCube(  .1, .25,  .1,   .1,   .1,   .1,255,255,  0); // yellow
+
+    createCube( -.2,  .3, -.4,   .1,   .2,  .15,  0,255,255); // cyan
+    createCube(  .1,-.25, -.1,   .1,   .1,   .1,255,  0,255); // magenta
+    createCube( -.3, -.1,  .2,   .1,   .1,   .1,  0,255,  0); // green
 
     update(gl);
 }
 
-function createCube(x, y, z, size, r, g, b){
+function createCube(x, y, z, l, w, h, r, g, b){
     //    v6----- v5
     //   /|      /|
     //  v1------v0|
@@ -96,14 +117,39 @@ function createCube(x, y, z, size, r, g, b){
         7, 4, 3,   7, 3, 2,    // down
         4, 7, 6,   4, 6, 5     // back
     ]);
+    // Iterate through all vertices
     for(let i = 0; i < cubeIndices.length; i++){
         let indexVal = cubeIndices[i];
-        g_Points.push( x + cubeVertices[indexVal][0] * size );
-        g_Points.push( y + cubeVertices[indexVal][1] * size );
-        g_Points.push( z + cubeVertices[indexVal][2] * size );
-        g_Points.push( r );
-        g_Points.push( g );
-        g_Points.push( b );
+        // push the x, y, z, r, g, b with appropriate transforms
+        vertexArr.push( x + cubeVertices[indexVal][0] * l );
+        vertexArr.push( y + cubeVertices[indexVal][1] * w );
+        vertexArr.push( z + cubeVertices[indexVal][2] * h );
+        vertexArr.push( r );
+        vertexArr.push( g );
+        vertexArr.push( b );
+    }
+
+    // cubes have 36 verts
+    vertsPerShapeArr.push(36);
+}
+
+function drawGeometry() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    let beginningIndex = 0; // number of vertices
+    // iterate per part
+    for(let i = 0; i < shapesPerPartArr.length; i++){
+        gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArr), gl.STATIC_DRAW);
+
+        let amountOfVerts = 0;
+        // iterate through all shapes in that part
+        for(let j = 0; j < shapesPerPartArr[i]; j++){
+            // obtain how many verts to draw
+            amountOfVerts += vertsPerShapeArr[j];
+        }
+        gl.drawArrays(gl.TRIANGLES, beginningIndex, amountOfVerts);
+        beginningIndex += amountOfVerts;
     }
 }
 
@@ -119,14 +165,14 @@ function getStorageLocations() {
         console.log('Failed to get the storage location of a_Color');
         return;
     }
-    u_TransformMatrix = gl.getUniformLocation(gl.program, 'u_TransformMatrix');
-    if (!u_TransformMatrix) {
-        console.log('Failed to get the storage location of u_TransformMatrix');
+    u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+    if (!u_ModelMatrix) {
+        console.log('Failed to get the storage location of u_ModelMatrix');
         return;
     }
 }
 
-// Code ised from MultiAttributeSize_Interleaved Ch. 5
+// Code used from MultiAttributeSize_Interleaved Ch. 5
 function initVertexBuffers() {
     var vertexBuffer = gl.createBuffer();
     if(!vertexBuffer){
@@ -136,7 +182,7 @@ function initVertexBuffers() {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); // multiple times
 
-    var FSIZE = vertices.BYTES_PER_ELEMENT;
+    var FSIZE = Float32Array.BYTES_PER_ELEMENT;
 
     gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0); // multiple times // mess with params for mutliple points
     gl.enableVertexAttribArray(a_Position); // multiple times
@@ -144,14 +190,17 @@ function initVertexBuffers() {
     gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
     gl.enableVertexAttribArray(a_Color);
 
-    gl.uniformMatrix4fv(u_TransformMatrix, false, transformMatrix.elements);
+    //gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
 }
+
 function update() {
+    rotationMatrix.setRotate(1,0,1,0);
+    modelMatrix.multiply(rotationMatrix);
+
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    gl.drawArrays(gl.TRIANGLES, 0, g_Points.length/6);
+    drawGeometry();
 
     requestAnimationFrame(update);
 }
