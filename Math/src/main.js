6704e69f77ed9b5ab0834aacd4b9af55d660a695
Rookirk <hwu24110@gmail.com>
2020-03-23 13:01:15 -0700

    Added sample scenarios so I can easily swap numbers

--- Math/src/main.js
+++ Math/src/main.js
@@ -1,6 +1,200 @@
 function main() {
-    const matrix = new Matrix4();
-    matrix.setRotate(30,0,1,0);
-    console.log(matrix);
-    printMatrix(matrix);
+
+	bilinearInterpolateColor();
+}
+
+// Quiz 2 a
+// GIven 3 vertices
+// find area of triangle
+function areaOfTriangle(){
+	const tri1 = new Vertex([1,0,2]);
+	const tri2 = new Vertex([1,0,1]);
+	const tri3 = new Vertex([-1,0,1]);
+	console.log(getTriangleArea(tri1,tri2,tri3));
+}
+
+// midterm 4
+// Given four points with colors and target, find color of target
+function bilinearInterpolateColor(){
+	const vertA = new VertexColor([14,21,0],[200,0,0]);
+    const vertB = new VertexColor([15,21,0],[40,0,80]);
+    const vertC = new VertexColor([14,20,0],[0,200,120]);
+    const vertD = new VertexColor([15,20,0],[0,200,240]);
+
+    const vertE = linearInterpolateVertexColor(vertA, vertC, new Vertex([14,20.5,0]));
+    const vertF = linearInterpolateVertexColor(vertB, vertD, new Vertex([15,20.5,0]));
+
+    const vertP = linearInterpolateVertexColor(vertE, vertF, new Vertex([14.75,20.5,0]));
+
+    printVertexColor(vertP);
+}
+
+// Quiz 2 b
+// Given 3 vertices
+// Find orthogonal vector
+function crossProduct(){
+	const tri1 = new Vertex([1,0,2]);
+	const tri2 = new Vertex([1,0,1]);
+	const tri3 = new Vertex([-1,0,1]);
+
+	const vec1 = createVector(tri1, tri2);
+	const vec2 = createVector(tri1, tri3);
+
+	const crossVec = getCrossProduct(vec1, vec2);
+	printVector(crossVec);
+}
+
+// Quiz 5 b
+// Given 2 vectors
+// find angle
+function normalizedDotProduct(){
+	const vec1 = new Vector3([1,3,0]);
+	const vec2 = new Vector3([1,2.3,0]);
+
+	vec1.normalize();
+	vec2.normalize();
+
+	console.log(getDotProduct(vec1,vec2));
+}
+
+// Quiz 6
+// Given vector find normal
+function normalizeVector(){
+	const vec1 = new Vector3([-3,-4,0]);
+	printVector(vec1);
+	vec1.normalize();
+	printVector(vec1);
+}
+
+// Quiz 1
+// GIven vector
+// Find magnitude
+function length(){
+	const vec1 = new Vector3([12,5,0]);
+	console.log(getMagnitude(vec1));
+}
+
+// midterm 3
+// Given two points with colors and target point Find the color of target
+function linearInterpolateColor(){
+	const vert1 = new VertexColor([1,5,0],[100,50,100]);
+    const vert2 = new VertexColor([5,13,0],[200,100,0]);
+
+    const partA = new Vertex([1.5,6,0]);
+    const partB = new Vertex([3,9,0]);
+    const partC = new Vertex([4.5,12,0]);
+
+    const resultA = linearInterpolateVertexColor(vert1, vert2, partA);
+    printVertexColor(resultA);
+    const resultB = linearInterpolateVertexColor(vert1, vert2, partB);
+    printVertexColor(resultB);
+    const resultC = linearInterpolateVertexColor(vert1, vert2, partC);
+    printVertexColor(resultC);
+}
+
+// Quiz 4
+// Given 2 matrices
+// find multiplication result
+function matrixMatrixMult(){
+	// matrix 1 is on the left
+	const elemMatrix1 = new Object();
+	elemMatrix1.elements = [
+		-1, -1, -3,  0,
+		 2,  2,  2,  0,
+		-1,  0,  1,  0,
+		 0,  0,  0,  1
+	];
+
+	const elemMatrix2 = new Object();
+	elemMatrix2.elements = [
+		-1,  2, -1,  0,
+		-1,  2,  0,  0,
+		 3,  2,  1,  0,
+		 0,  0,  0,  1
+	];
+
+	const matrix1 = new Matrix4(elemMatrix1);
+	const matrix2 = new Matrix4(elemMatrix2);
+
+	matrix1.multiply(matrix2);
+	printMatrix(matrix1);
+}
+
+// Quiz 3
+// Given matrix and vector
+// FInd solution
+// On paper, matrix row is equal to on code matrix column
+function matrixVectorMult(){
+	const elemMatrix = new Object();
+	elemMatrix.elements = [
+		-1, -1, -3,  0,
+		 2,  2,  2,  0,
+		-1,  0,  1,  0,
+		 0,  0,  0,  1
+	];
+	const matrix = new Matrix4(elemMatrix);
+	const vector = new Vertex([-1, 4, 0]);
+
+	const returnVec = transformVertex(vector, matrix);
+	printVertex(returnVec);
+}
+
+// midterm 6
+// GIven 3 matrices and apoint, transform
+function matrixTransform(){
+	// matrix transformations
+
+    // T*R*S*V
+    // In that order
+    const translateMatrix = new Matrix4();
+    translateMatrix.setTranslate(0,3,0);
+
+    const rotateMatrix = new Matrix4();
+    rotateMatrix.setRotate(90,0,0,1);
+
+    const scaleMatrix = new Matrix4();
+    scaleMatrix.setScale(1,2,1);
+
+    const vertex = new Vertex([5,2,0]);
+
+    const transformMatrix = new Matrix4();
+    transformMatrix.multiply(translateMatrix);
+    transformMatrix.multiply(rotateMatrix);
+    transformMatrix.multiply(scaleMatrix);
+
+    const returnVertex = transformVertex(vertex, transformMatrix);
+    printVertex(returnVertex);
+}
+
+// midterm 7
+// Given 3 matrices and 3 points, transform
+function matrixTransform2(){
+	const matrixA = new Matrix4();
+    matrixA.setTranslate(-4,-1,0);
+
+    const matrixB = new Matrix4();
+    matrixB.setScale(-1,1.5,0);
+
+    const matrixC = new Matrix4();
+    matrixC.setTranslate(3,2,0);
+
+    const vertex1 = new Vertex([6,1,0]);
+    const vertex2 = new Vertex([4,1,0]);
+    const vertex3 = new Vertex([4,3,0]);
+
+    // transltate -4,-1 first
+    // scale -1, 1.5 second
+    // translate 3,2 last
+    // so reverse order
+    const transformMatrix = new Matrix4();
+    transformMatrix.set(matrixC);
+    transformMatrix.multiply(matrixB);
+    transformMatrix.multiply(matrixA);
+
+    const returnVertex1 = transformVertex(vertex1, transformMatrix);
+    printVertex(returnVertex1);
+    const returnVertex2 = transformVertex(vertex2, transformMatrix);
+    printVertex(returnVertex2);
+    const returnVertex3 = transformVertex(vertex3, transformMatrix);
+    printVertex(returnVertex3);
 }
--- Math/src/matrixFuncs.js
+++ Math/src/matrixFuncs.js
@@ -1,46 +1,75 @@
+const defaultDecimalPrecision = 2;
+
 class Vertex {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    constructor(coords) {
+        this.x = coords[0];
+        this.y = coords[1];
+        this.z = coords[2];
     }
 }
 
-function printMatrix(matrix) {
+class VertexColor {
+    constructor(coords, color) {
+        this.x = coords[0];
+        this.y = coords[1];
+        this.z = coords[2];
+
+        this.r = color[0];
+        this.g = color[1];
+        this.b = color[2];
+    }
+}
+
+function printMatrix(matrix, precision = defaultDecimalPrecision) {
     let string = "";
     for(let i = 0; i < 4; i++){
-        string += matrix.elements[i].toFixed(2) + " , " +
-                  matrix.elements[i+4].toFixed(2) + " , " +
-                  matrix.elements[i+8].toFixed(2) + " , " +
-                  matrix.elements[i+12].toFixed(2) + " ,\n"
+        string += matrix.elements[i].toFixed(precision) + " , " +
+                  matrix.elements[i+4].toFixed(precision) + " , " +
+                  matrix.elements[i+8].toFixed(precision) + " , " +
+                  matrix.elements[i+12].toFixed(precision) + " ,\n"
     }
     console.log(string);
 }
 
-function printVector(vector) {
-    console.log("[ " + vector.elements[0].toFixed(2) + " , " +
-                       vector.elements[1].toFixed(2) + " , " +
-                       vector.elements[2].toFixed(2) + " ]");
+function printVector(vector, precision = defaultDecimalPrecision) {
+    console.log("[ " + vector.elements[0].toFixed(precision) + " , " +
+                       vector.elements[1].toFixed(precision) + " , " +
+                       vector.elements[2].toFixed(precision) + " ]");
+}
+
+function printVertex(vertex, precision = defaultDecimalPrecision) {
+    console.log("[ " + vertex.x.toFixed(precision) + " , " +
+                       vertex.y.toFixed(precision) + " , " +
+                       vertex.z.toFixed(precision) + " ]");
+}
+
+function printVertexColor(vertex, precision = defaultDecimalPrecision) {
+    console.log("coords: [ " + vertex.x.toFixed(precision) + " , " +
+                       vertex.y.toFixed(precision) + " , " +
+                       vertex.z.toFixed(precision) + " ]\n" +
+                "colors: [ " + vertex.r.toFixed(precision) + " , " +
+                       vertex.g.toFixed(precision) + " , " +
+                       vertex.b.toFixed(precision) + " ]\n");
 }
 
 function createVector(origin, arrow) {
-    return new Vector([
-        arrow.elements[0] - origin.elements[0],
-        arrow.elements[1] - origin.elements[1],
-        arrow.elements[2] - origin.elements[2]
+    return new Vector3([
+        arrow.x - origin.x,
+        arrow.y - origin.y,
+        arrow.z - origin.z
     ]);
 }
 
 function transformVertex(vertex, transformMatrix){
-    const oldVertex = new Vector3(vertex);
+    const oldVertex = new createVector(new Vertex([0,0,0]), vertex);
     const newMatrix = new Matrix4();
     newMatrix.set(transformMatrix);
     const returnMatrix = newMatrix.multiplyVector3(oldVertex);
 
-    return [returnMatrix.elements[0],
-            returnMatrix.elements[1],
-            returnMatrix.elements[2]
-    ];
+    return new Vertex([returnMatrix.elements[0],
+                       returnMatrix.elements[1],
+                       returnMatrix.elements[2]
+    ]);
 }
 
 function getCrossProduct(vector1, vector2) {
@@ -67,14 +96,14 @@ function getCrossProduct(vector1, vector2) {
 }
 
 function getDotProduct(vec1, vec2){
-    const vector1 = new Vertex(
+    const vector1 = new Vertex([
         vec1.elements[0],
         vec1.elements[1],
-        vec1.elements[2]);
-    const vector2 = new Vertex(
+        vec1.elements[2]]);
+    const vector2 = new Vertex([
         vec2.elements[0],
         vec2.elements[1],
-        vec2.elements[2]);
+        vec2.elements[2]]);
 
     return (vector1.x * vector2.x) + (vector1.y * vector2.y) + (vector1.z * vector2.z);
 }
@@ -93,15 +122,29 @@ function getTriangleArea(vert1, vert2, vert3){
 
     const crossVec = getCrossProduct(vec1, vec2);
 
-    return getMagnitude(crossVec);
+    return getMagnitude(crossVec)/2;
 }
 
 function linearMap(value, inputLow, inputHigh, outputLow, outputHigh) {
     return outputLow + (outputHigh - outputLow) * (value - inputLow) / (inputHigh - inputLow);
 }
 
-function linearInterpolate(){
+function linearInterpolateVertexColor(vertColor1, vertColor2, targetVert){
+    const totalDistance = getMagnitude(createVector(vertColor1, vertColor2));
 
+    const fractionalDistance = getMagnitude(createVector(vertColor1, targetVert));
+
+    const influence = fractionalDistance / totalDistance;
+
+    const r = linearInterpolate( influence, vertColor1.r, vertColor2.r );
+    const g = linearInterpolate( influence, vertColor1.g, vertColor2.g );
+    const b = linearInterpolate( influence, vertColor1.b, vertColor2.b );
+
+    return new VertexColor([targetVert.x, targetVert.y, targetVert.z], [r,g,b]);
+}
+
+function linearInterpolate( influence, value1, value2 ){
+    return (1 - influence) * value1 + influence * value2;
 }
 
 function barycentricInterpolate(){
